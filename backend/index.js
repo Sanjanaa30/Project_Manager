7b331418d5b5af93dd9995b676e54e211a0fe062
Sanjanaa30 <sshivanand@binghamton.edu>
2025-10-03 16:53:18 -0400

    Backend Setup

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the TaskHub API" });
+});
+
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({ message: "Internal Server Error" });
+});
+
+//not found middleware
+app.use((req, res) => {
+    res.status(404).json({ message: "Message not found" });
+});
+
+// Connect to Morgan
+app.use(morgan("dev"));
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
